23d30cb2289eba65198d45952e7b490222baeee4
Simon Guest <simon.guest@tesujimath.org>
2025-01-01 11:29:32 +1300

    Remove plugin (#44)
    Plugin has been deprecated for some time. It has been replaced by the
    module.

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    rust-overlay.url = "github:oxalica/rust-overlay";
-
     flake-utils.url = "github:numtide/flake-utils";
 
     bash-env-json = {
@@ -14,17 +12,15 @@
     };
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, bash-env-json, ... }:
+  outputs = { nixpkgs, flake-utils, bash-env-json, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          overlays = [ (import rust-overlay) ];
-
           pkgs = import nixpkgs {
-            inherit system overlays;
+            inherit system;
           };
 
-          inherit (pkgs) lib rust-bin stdenvNoCC writeShellScriptBin;
+          inherit (pkgs) lib stdenvNoCC;
           flakePkgs = {
             bash-env-json = bash-env-json.packages.${system}.default;
           };
@@ -44,52 +40,6 @@
                 runHook postBuild
               '';
             };
-
-          # cargo-nightly based on https://github.com/oxalica/rust-overlay/issues/82
-          nightly = rust-bin.selectLatestNightlyWith (t: t.default);
-          cargo-nightly = writeShellScriptBin "cargo-nightly" ''
-            export RUSTC="${nightly}/bin/rustc";
-            exec "${nightly}/bin/cargo" "$@"
-          '';
-
-          nu_plugin_bash_env =
-            let
-              inherit (builtins) fromTOML readFile;
-              inherit (pkgs) lib makeWrapper rustPlatform;
-              cargoConfig = fromTOML (readFile ./Cargo.toml);
-            in
-            rustPlatform.buildRustPackage
-              {
-                pname = "nu_plugin_bash_env";
-                version = cargoConfig.package.version;
-
-                src = ./.;
-
-                cargoLock = {
-                  lockFile = ./Cargo.lock;
-                };
-
-                meta = with lib; {
-                  description = "A Bash environment plugin for Nushell";
-                  homepage = "https://github.com/tesujimath/nu_plugin_bash_env";
-                  license = licenses.mit;
-                  maintainers = [ maintainers.tailhook ];
-                };
-
-                buildInputs = [
-                  makeWrapper
-                  flakePkgs.bash-env-json
-                ];
-
-
-                preBuild = ''
-                  export NIX_BASH_ENV_JSON=${flakePkgs.bash-env-json}/bin/bash-env-json
-                '';
-
-                postFixup = ''
-                  wrapProgram $out/bin/nu_plugin_bash_env --set NU_PLUGIN_BASH_ENV_JSON ${flakePkgs.bash-env-json}/bin/bash-env-json
-                '';
-              };
         in
         {
           devShells.default =
@@ -97,27 +47,19 @@
               inherit (pkgs)
                 mkShell
                 bashInteractive
-                jq
-                cargo-modules
-                cargo-udeps
-                rust-bin;
+                jq;
             in
 
             mkShell {
               nativeBuildInputs = [
                 bashInteractive
                 jq
-                cargo-modules
-                cargo-nightly
-                cargo-udeps
-                rust-bin.stable.latest.default
               ];
             };
 
           packages = {
             default = bash-env-module-with-bash-env-json;
             module = bash-env-module-with-bash-env-json;
-            plugin = nu_plugin_bash_env;
           };
         }
       );
